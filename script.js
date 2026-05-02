625c2c5de23b2176cd5950488b72a5ac5f57a23e
Jônatas <jonatas.webj@gmail.com>
2026-05-02 09:24:59 -0400

    Alert quando o valor max for <= que o valor min

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
- function generateNumber() {
-   
+function generateNumber() {
+
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
- 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert(`O número sorteado foi: ${result}`)
+
+    if (max <= min) {
+        alert("O segundo valor deve ser MAIOR que o primeiro.")
+        return;
+    }
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(`O número sorteado foi: ${result}`)
+    }
+
+
+
 }
